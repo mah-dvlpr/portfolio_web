3da88e07503e6a01c7a1d727944c191a4f4b0b56
Marcus Hanson <mah.dvlpr@gmail.com>
2021-02-28 09:48:08 +0100

    Added a physics delegate, but it does not seem to work correctly.

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -10,20 +12,25 @@ class BackdropAnimation extends StatefulWidget {
 class _BackdropAnimationState extends State<BackdropAnimation>
     with SingleTickerProviderStateMixin {
   AnimationController _animationController;
-  StreamController<List<Point>> _streamController;
-  List<Point> _points;
+  StreamController<List<_Point>> _streamController;
+
+  static const int pointsMax = 50;
+  List<_Point> _points;
 
   @override
   void initState() {
     super.initState();
-    // TODO: The framework's unbounded variant of AnimationController is a bit 
+    // TODO: The framework's unbounded variant of AnimationController is a bit
     // odd as of now (1.27.0-4.0.pre). Doing this instead. V
-    _animationController = AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
     _animationController.addListener(() {
       _notifyPainter();
     });
-    _streamController = StreamController<List<Point>>();
-    _points = <Point>[];
+    _streamController = StreamController<List<_Point>>();
+    _points = <_Point>[];
+
+    _generateRandomPoints(_points);
 
     // Start animation
     _animationController.repeat();
@@ -31,10 +38,10 @@ class _BackdropAnimationState extends State<BackdropAnimation>
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<Point>>(
+    return StreamBuilder<List<_Point>>(
       stream: _streamController.stream,
       builder: (_, snapshot) => CustomPaint(
-        painter: BackdropPainter(snapshot.data),
+        painter: _BackdropPainter(snapshot.data),
         willChange: true,
       ),
     );
@@ -47,27 +54,105 @@ class _BackdropAnimationState extends State<BackdropAnimation>
     super.dispose();
   }
 
-  _notifyPainter() {
-    _points.add(Point());
+  void _notifyPainter() {
+    // _PhysicsDelegate.updatePoints(_points);
     _streamController.add(_points);
+  }
+
+  /// Has to be called AFTER the first paint.
+  void _generateRandomPoints(List<_Point> points) {
+    for (int i = 0; i < pointsMax; ++i) {
+      _points.add(_Point.getRandomPoint());
+    }
   }
 }
 
-class BackdropPainter extends CustomPainter {
-  List<Point> _points;
+class _BackdropPainter extends CustomPainter {
+  List<_Point> _points;
 
-  BackdropPainter(this._points);
+  _BackdropPainter(this._points);
 
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
-    print('hej');
+    canvas.drawRect(Rect.largest, Paint()..color = Colors.cyan[900]);
+
+    for (var point in _points) {
+      canvas.drawCircle(point.position, point.size, point.pointBrush);
+    }
   }
 
   @override
-  bool shouldRepaint(BackdropPainter oldDelegate) {
+  bool shouldRepaint(_BackdropPainter oldDelegate) {
     return true;
   }
 }
 
-class Point {}
+class _Point {
+  /// TODO: Arbitray value for now...
+  static const speedMax = 50;
+  static const sizeMax = 4;
+  static final random = Random();
+
+  final pointBrush = Paint()..color = Colors.lightBlue[50];
+
+  /// Current position on canvas.
+  Offset position;
+
+  /// Speed (+/-) in terms of x/y coordinates of current position on canvas.
+  Offset speed;
+
+  double size;
+
+  _Point(this.position, this.speed, this.size);
+
+  static _Point getRandomPoint() {
+    return _Point(
+        Offset(random.nextDouble() * window.physicalSize.width,
+            random.nextDouble() * window.physicalSize.height),
+        Offset(random.nextDouble() * speedMax, random.nextDouble() * speedMax),
+        random.nextDouble() * sizeMax);
+  }
+}
+
+/// Utility class for handling physics of supplied points.
+class _PhysicsDelegate {
+  static final double G = 6.67384 * pow(10, -11);
+  static DateTime dateTime;
+
+  _PhysicsDelegate._();
+
+  static updatePoints(List<_Point> points) {
+    if (dateTime != null && DateTime.now().difference(dateTime).inSeconds < 1) {
+      return;
+    }
+    dateTime = DateTime.now();
+    _updatePointSpeedPerAdjacentPoints(points);
+    _updatePointPosition(points);
+  }
+
+  static _updatePointSpeedPerAdjacentPoints(List<_Point> points) {
+    for (int currentPointIndex = 0; currentPointIndex < points.length - 1; ++currentPointIndex) {
+      var currentPoint = points[currentPointIndex];
+      double currentPointMass = currentPoint.size; // TODO: Doing this for now, might change later
+      for (int otherPointIndex = currentPointIndex + 1; otherPointIndex < points.length; ++otherPointIndex) {
+        var otherPoint = points[otherPointIndex];
+        double otherPointMass = points[otherPointIndex].size; // TODO: Doing this for now, might change later
+
+        var attraction = G * currentPointMass * otherPointMass / _hypotenuseSquared(currentPoint, otherPoint);
+        currentPoint.speed += Offset(attraction, attraction);
+      }
+    }
+  }
+
+  static _updatePointPosition(List<_Point> points) {
+    for (var point in points) {
+      point.position += point.speed;
+    }
+  }
+
+  static double _hypotenuseSquared(_Point a, _Point b) {
+    var dx = pow((a.position.dx - b.position.dx).abs(), 2);
+    var dy = pow((a.position.dy - b.position.dy).abs(), 2);
+    return dx + dy;
+  }
+}
